e96faa363114d31c663115f569c8fb565d7c4ad3
Joanne Healy <jolguk@hotmail.co.uk>
2024-11-12 15:35:37 +0000

    fixed duplicate chat

--- src/ContosoSuitesWebAPI/Program.cs
+++ src/ContosoSuitesWebAPI/Program.cs
@@ -109,15 +109,7 @@ app.MapGet("/", async () =>
      .WithName("GetRecentBookingsForHotel")
      .WithOpenApi();
 
-// This endpoint is used to send a message to the Azure OpenAI endpoint.
-app.MapPost("/Chat", async Task<string> (HttpRequest request) =>
-{
-    var message = await Task.FromResult(request.Form["message"]);
-    
-    return "This endpoint is not yet available.";
-})
-    .WithName("Chat")
-    .WithOpenApi();
+
 
 // This endpoint is used to vectorize a text string.
 // We will use this to generate embeddings for the maintenance request text.
