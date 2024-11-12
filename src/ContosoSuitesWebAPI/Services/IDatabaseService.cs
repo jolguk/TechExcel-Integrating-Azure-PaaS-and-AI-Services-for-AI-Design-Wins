ecd7e0f0de94036e2a702291f7ae943372314835
Joanne Healy <jolguk@hotmail.co.uk>
2024-11-12 15:31:47 +0000

    spacing

--- src/ContosoSuitesWebAPI/Services/IDatabaseService.cs
+++ src/ContosoSuitesWebAPI/Services/IDatabaseService.cs
@@ -8,7 +8,7 @@ public interface IDatabaseService
     Task<IEnumerable<Booking>> GetBookingsForHotel(int hotelId);
     Task<IEnumerable<Booking>> GetBookingsByHotelAndMinimumDate(int hotelId, DateTime dt);
 
-     Task<IEnumerable<Booking>> GetBookingsMissingHotelRooms();
-     Task<IEnumerable<Booking>> GetBookingsWithMultipleHotelRooms();
+    Task<IEnumerable<Booking>> GetBookingsMissingHotelRooms();
+    Task<IEnumerable<Booking>> GetBookingsWithMultipleHotelRooms();
 
 }
